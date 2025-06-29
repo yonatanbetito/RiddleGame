14b4e5ec33dd374d3631ea9839def2959480c31d
yonatan_betito <yonatanbetito@gmail.com>
2025-06-29 15:48:58 +0300

    run second

--- RiddleGame/TheGame.js
+++ RiddleGame/TheGame.js
@@ -1,28 +1,21 @@
 import readlineSync from "readline-sync";
 import { Player } from "./classes/Player.js";
-import { Riddle } from "./classes/Riddle.js"; // שכחת לייבא
+import { Riddle } from "./classes/Riddle.js";
 import r1 from "./Riddles/r1.js";
-
 
 const name = readlineSync.question("enter your name: ");
 const player = new Player(name);
 const startTime = Date.now();
-const riddle = new Riddle(r1)
+const riddle = new Riddle(r1);
 console.log(riddle.taskDescription);
-riddle.ask()
+
+riddle.ask();
 const endTime = Date.now();
-player.recordTime(startTime,endTime);
+player.recordTime(startTime, endTime);
 const stats = player.showStats();
 console.log(`Great job ${player.name}, you finished the game!`);
 console.log(`Total time: ${stats.total}`);
 console.log(`Average per riddle: ${stats.average}`);
-
-
-
-
-
-
-
 
 // for (const riddleObj of riddlesData) {
 //   const riddle = new Riddle(riddleObj);
